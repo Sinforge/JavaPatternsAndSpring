18742dc1572c04274da3348959e549660a4d13df
Sinforge <vlad.vlasov77@mail.ru>
2023-02-27 23:49:22 +0300

    fix problem with practice9

---
+++ new_pract9/src/main/java/com/example/new_pract9/NewPract9Application.java
@@ -0,0 +1,13 @@
+package com.example.new_pract9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPract9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewPract9Application.class, args);
+    }
+
+}
---
+++ new_pract9/src/test/java/com/example/new_pract9/NewPract9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_pract9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPract9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
